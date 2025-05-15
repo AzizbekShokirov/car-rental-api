fe9194045038dfaf0675f8944eeb587ed7b9da86
AzizbekShokirov <azizbekshokirov.dev@gmail.com>
2025-05-15 18:37:07 +0500

    Minor changes

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     # API schema and documentation
-    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
